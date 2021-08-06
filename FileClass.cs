d77cdb0933d33618394584f0a2a5841285129a54
sadhik98 <88191661+sadhik98@users.noreply.github.com>
2021-08-06 16:13:53 +0530

    Add files via upload
    Model's class file for the purpose of the Study Material.

---
+++ FileClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolOnlineExamination.Models
+{
+    public class FileClass
+    {
+        public int FileId { get; set; } = 0;
+
+        public string Name { get; set; } = "";
+
+        public string path { get; set; } = "";
+
+        public List<FileClass> Teacher { get; set; } = new List<FileClass>();
+    }
+}
