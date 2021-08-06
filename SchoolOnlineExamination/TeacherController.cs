55f79c9c618141f946c6e4eb1ea5743ee7d09e58
sadhik98 <88191661+sadhik98@users.noreply.github.com>
2021-08-06 16:11:01 +0530

    Add files via upload
    Teacher Controller for the purpose of Study Material.

---
+++ SchoolOnlineExamination/TeacherController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolOnlineExamination.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolTestProject.Controllers
+{
+    [Authorize(Roles = "Teacher,Guest1")]
+    public class TeacherController : Controller
+    {
+        IHostingEnvironment _hostingEnvironment = null;
+        public TeacherController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpGet]
+        public IActionResult Index(string filename = "")
+        {
+            FileClass fileObj = new FileClass();
+            fileObj.Name = filename;
+
+            string path = $"{_hostingEnvironment.WebRootPath}\\Documents\\";
+            int nId = 1;
+
+            foreach (string pdfPath in Directory.EnumerateFiles(path, "*.pdf"))
+            {
+                fileObj.Teacher.Add(new FileClass()
+                {
+                    FileId = nId++,
+                    Name = Path.GetFileName(pdfPath),
+                    path = pdfPath
+                });
+            }
+
+            return View(fileObj);
+        }
+        [HttpPost]
+
+        public IActionResult Index(IFormFile file, [FromServices] IHostingEnvironment hostingEnvironment)
+        {
+            string fileName = $"{hostingEnvironment.WebRootPath}\\Documents\\{file.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+            return Index();
+        }
+
+        public IActionResult PDFViewerNewTab(string fileName)
+        {
+            string path = _hostingEnvironment.WebRootPath + "\\Documents\\" + fileName;
+            return File(System.IO.File.ReadAllBytes(path), "application/pdf");
+        }
+    }
+}
